3aa4af03b03448ed44ee37ef574dd0f568041042
Frodo <ingrnrg@gmail.com>
2022-05-30 21:29:11 -0500

    Master Encoding

---
+++ intro_js/01-SalidaEntradas/js/main.js
@@ -0,0 +1,24 @@
+//Formas de mostrar datos al usuario 
+
+// 1.- console.log
+console.log('Hola Mundo');
+console.log(1994);
+console.log(2 + 4);
+console.log(10 < 6);
+
+// 2.- document.write
+document.write ('Hola Mundo');
+
+// 3.- Alert
+alert ('popup');
+
+// Solicitar datos al usuario fianl
+// 1. prompt
+var nombre = prompt('Ingresa tu nombre');
+
+
+/* la ejecucion de codigo se realiza del parentesis mas interior hacia afuera */
+
+alert(prompt('Ingrese tu edad'));
+
+document.write(nombre);
