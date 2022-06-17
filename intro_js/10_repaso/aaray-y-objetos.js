ad1109f1e5c8740657ed46f704726e42e66643d3
Frodo <ingrnrg@gmail.com>
2022-06-16 20:58:09 -0500

    repaso

---
+++ intro_js/10_repaso/aaray-y-objetos.js
@@ -0,0 +1,32 @@
+const usuario = [
+    {
+        id: 0,
+        nombre: "",
+        edad: null,
+        esAdulto: true,
+    }
+    {
+        id: 1,
+        nombre: "",
+        edad: null,
+        esVip: true,
+    }
+    {
+        id: 3,
+        nombre: "",
+        edad: null,
+    }
+];
+
+//Modificar el segundo nombre
+//indexOf
+//findIndex
+//find
+
+function validador()
+{
+    return element.id ===2;
+}
+
+const index = usuario.findIndex(validador)
+usuario[index].nombre = "Carlos";
