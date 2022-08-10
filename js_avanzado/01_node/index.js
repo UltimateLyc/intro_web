636d1c0e893c698a46737290b36147b85ac665de
Frodo <ingrnrg@gmail.com>
2022-08-09 19:38:59 -0500

    js avanzado

---
+++ js_avanzado/01_node/index.js
@@ -0,0 +1,16 @@
+console.log('Estoy escribiendo desde la terminal')
+var colors  = require("colors")
+
+function  Sumar (x,y){
+	let suma = x+y
+	console.log("la suma es:" .red +suma)
+
+}
+
+Sumar (30,40)
+
+
+for (let i=0; i<5; i++)
+{
+	console.log("Mabel".magenta)
+}
---
+++ js_avanzado/02_Async/index.js
@@ -0,0 +1,4 @@
+console.log("02 Asincronicidad")
+
+//Codigo secuencuencial (sincrono)
+
