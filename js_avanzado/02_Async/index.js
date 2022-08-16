2dba5b9113c8ecaa80c57d7b3a3a5c52ba10f193
Frodo <ingrnrg@gmail.com>
2022-08-16 18:49:26 -0500

    API NASA

--- js_avanzado/02_Async/index.js
+++ js_avanzado/02_Async/index.js
@@ -2,3 +2,86 @@ console.log("02 Asincronicidad")
 
 //Codigo secuencuencial (sincrono)
 
+console.log(1)
+console.log(2)
+console.log(3)
+console.log("************************")
+//Callback function
+
+let alumnos = 
+[
+    "Sebastian",
+    "Aracely",
+    "Carlos",
+    "Mabel",
+    "Rene"
+]
+
+function funcionpara (elemento,indice,arreglo)
+{
+    console.log(elemento);
+}
+
+alumnos.forEach (funcionpara);
+
+/* alumnos.forEach((elemento,indice,arreglo) => {
+    console.log(elemento)
+}) */
+
+console.log("************************")
+
+const primerafuncion = () => {
+    console.log('Esta es la primera funcion');
+}
+
+const segundafuncion = () => {
+    primerafuncion();
+    console.log('Esta es la segunda funcion');
+}
+
+const terceraafuncion = () => {
+    segundafuncion();
+    console.log('Esta es la tercera funcion');
+}
+
+terceraafuncion();
+
+console.log("************************")
+
+const uno = () =>
+{
+    return 50;
+}
+
+const dos = () =>
+{
+    return 40;
+}
+
+const sumaFunciones = (funcion1, funcion2) => {
+    const suma = funcion1() + funcion2();
+    return suma;
+}
+console.log(sumaFunciones(uno,dos))
+
+
+//settimeout es para ejecutar  una funcion despues de un tiempo
+console.log("Antes del settime")
+
+setTimeout(()=>
+{
+    console.log("************************")
+},4000)
+console.log("Rdolfo")
+
+//Simulacion de cuello de botella
+
+console.log("Rene");
+setTimeout(()=>{
+	return console.log("flor");
+},4000)
+
+for(let i=0; i < 9999999999; i++);
+
+console.log("final");
+
---
+++ js_avanzado/03_API_Nasa/index.js
@@ -0,0 +1,43 @@
+console.log('API de la NASA');
+import fetch from "node-fetch";
+const key = 'QbohNMZcTf3ZNi4GPxnBnPpCKocqtYacoaPF8aRw';
+
+let urlapi = `https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=${key}`;
+
+const respuestaApi = await fetch(urlapi); //Cuando revisamos un api usamos await para que esta tenga el tiempo de buscar y luego almacenarla
+/* console.log(respuestaApi) */
+
+const respuestaJson = await respuestaApi.json(); //Guardamos el JSON en una variable 
+/* console.log(respuestaJson); */
+
+let meteoritos = respuestaJson.near_earth_objects;/*['2015-09-08'];//Harcode */ //Leemos el objeto y lo guardamos 
+/* console.log(meteoritos) */ //Imprime todo el JSON
+
+Object.keys(meteoritos).forEach((elemento, indice, arreglo) =>{
+    let listaXdia = meteoritos[elemento]
+    //Con for normal
+    /* for(let i = 0; i < listaXdia.length; i++)
+    {
+        if(listaXdia[i].is_potentially_hazardous_asteroid)
+        {
+            console.log(`El meteorito ${listaXdia[i].name} es potencialmente peligroso`)
+        }
+        else
+        {
+            console.log(`Este metiorito ${listaXdia[i].name} no es peligroso`)
+        }
+    } */
+
+    //con for each
+    listaXdia.forEach((elemento,indice, arreglo)=>{
+        if(elemento.is_potentially_hazardous_asteroid)
+        {
+            console.log(`El meteorito ${elemento.name} es potencialmente peligroso. El diametro estimado es: ${(((elemento.estimated_diameter.meters.estimated_diameter_min)+(elemento.estimated_diameter.meters.estimated_diameter_max))/2).toFixed(2)} metros`)
+        }
+        else
+        {
+            console.log(`Este metiorito ${elemento.name} no es peligroso. El diametro estimado es: ${(((elemento.estimated_diameter.meters.estimated_diameter_min)+(elemento.estimated_diameter.meters.estimated_diameter_max))/2).toFixed(2)} metros`)
+        }
+    })
+}) //De esta manera recorremos las llaves (fechas) de manera automatica y nos imprime los elementos 
+//ya que no podemos mandar el ID: 0 ya que es un JSON y el ID nuevo es la "fecha"
