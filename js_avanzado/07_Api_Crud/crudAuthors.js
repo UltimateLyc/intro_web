8d6cf823e913a325e210ce88966f96e863f66847
Frodo <ingrnrg@gmail.com>
2022-08-24 19:37:07 -0500

    Ejecutar a getAuthor

--- js_avanzado/07_Api_Crud/crudAuthors.js
+++ js_avanzado/07_Api_Crud/crudAuthors.js
@@ -1,1 +1,47 @@
-//Vamos a tener la logica
+// Vamos a tener la logica
+// Documentacion: https://goodreads-devf-aaron.herokuapp.com/docs/
+// URI de la API: https://goodreads-devf-aaron.herokuapp.com/api/v1/
+
+//1) Traer a request que acabamos de instalar
+
+const request = require ('request')
+
+//2) Traer nuestro endpiont
+const URI = 'https://goodreads-devf-aaron.herokuapp.com/api/v1/authors/'
+
+// Lista a todos los autores (authors)
+const listAuthors = () => {
+    request.get(URI,(error, response, body) => {
+        if(response.statusCode === 200)
+        {
+            const authors = JSON.parse(body)
+            console.log(authors)
+        }
+        else
+        {
+            console.log(response.statusCode , response.statusMessage)
+                    //          404             Not Found
+        }
+    })
+}
+
+const getAuthors = (id) => {
+    request.get(URI+id+'/',(error, response, body) => {
+        if(response.statusCode === 200)
+        {
+            const author = JSON.parse(body)
+            console.log(author)
+        }
+        else
+        {
+            console.log('ID NO VALIDO: ',response.statusCode , response.statusMessage)
+                                //          404             Not Found
+        }
+    })
+}
+
+
+module.exports = {
+    listAuthors,
+    getAuthors
+}
--- js_avanzado/07_Api_Crud/main.js
+++ js_avanzado/07_Api_Crud/main.js
@@ -1,1 +1,9 @@
 //Ejecutar mis bloques de codigo
+//Main es nuestro archivo js principal
+const goodReadsCrud = require('./crudAuthors')
+
+//Lsita de todos los autores
+//goodReadsCrud.listAuthors()
+
+//Lista a un autor por su ID
+goodReadsCrud.getAuthors(14639)
