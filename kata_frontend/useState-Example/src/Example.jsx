1f0bc9f4d9afe972f8175925c9741da1fd876ed4
Rafael Ramos <ingrnrg@gmail.com>
2022-09-25 10:27:19 -0500

    Example React useState

---
+++ kata_frontend/useState-Example/src/Example.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'React'
+
+const Example = () => {
+
+    const [count, setCount] = useState (0)
+
+  return (
+    <div>
+        <h1>Example useState</h1>
+        <p>You clicked {count} times</p>
+        <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  )
+}
+
+export default Example
